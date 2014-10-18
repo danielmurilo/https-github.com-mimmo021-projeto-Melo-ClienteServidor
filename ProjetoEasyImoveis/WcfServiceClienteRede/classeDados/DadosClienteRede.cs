ca395dbee9128daf5315d42dd112f321cf454a83
danielmurilo <danielmurilo@yahoo.com.br>
2014-10-18 14:16:55 -0300

    Dados Cliente Rede
    Dados Cliente Rede

---
+++ ProjetoEasyImoveis/WcfServiceClienteRede/classeDados/DadosClienteRede.cs
@@ -0,0 +1,111 @@
+﻿using ProjetoEasyImoveis.classesconexao;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WcfServiceClienteRede.classebasica;
+
+namespace WcfServiceClienteRede.classeDados
+{
+    public class DadosClienteRede
+    {
+        private conexao conn = new conexao();
+        //construtor
+        public DadosClienteRede()
+        {
+            this.conn.abrirconexao();
+        }
+
+
+        public void cadastrarClienteRede(ClienteRede cr)
+        {
+            try
+            {
+                string sql = "insert into Cli_Rede(nome_Cli_Rede, login_Cli_Rede, senha_Cli_Rede, email_Cli_Rede, telefone_Cli_Rede) values (@nome, @login, @senha, @email, @telefone);";
+                SqlCommand cadastrando = new SqlCommand(sql, conn.sqlconn);
+
+                cadastrando.Parameters.AddWithValue("@nome", cr.Nome_cli_rede);
+                cadastrando.Parameters.AddWithValue("@login", cr.Login_cli_rede);
+                cadastrando.Parameters.AddWithValue("@senha", cr.Senha_cli_rede);
+                cadastrando.Parameters.AddWithValue("@email", cr.Email_cli_rede);
+                cadastrando.Parameters.AddWithValue("@telefone", cr.Telefone_cli_rede);
+                conn.fecharConexao();
+                conn.sqlconn.Dispose();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Não foi possivel cadastrar Usuário no Banco!!");
+            }
+        }
+        public void alterarClienteRede(ClienteRede cr)
+        {
+            try
+            {
+                string sql = "update Cli_Rede set nome_Cli_Rede = @nome, login_Cli_Rede = @login, senha_Cli_Rede = @senha, email_Cli_Rede = @email, telefone_Cli_Rede = @telefone);";
+                SqlCommand update = new SqlCommand(sql, conn.sqlconn);
+                update.Parameters.AddWithValue("@nome", cr.Nome_cli_rede);
+                update.Parameters.AddWithValue("@login", cr.Login_cli_rede);
+                update.Parameters.AddWithValue("@senha", cr.Senha_cli_rede);
+                update.Parameters.AddWithValue("@email", cr.Email_cli_rede);
+                update.Parameters.AddWithValue("@telefone", cr.Telefone_cli_rede);
+                conn.fecharConexao();
+                conn.sqlconn.Dispose();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Não foi possivel atualizar registro do Usuário!!");
+            }
+        }
+        public void deletarClienteRede(ClienteRede cr)
+        {
+            try
+            {
+                string sql = "delete from Cli_Rede where Id_Cli = @Id_Cli_Rede";
+                SqlCommand deletar = new SqlCommand(sql, conn.sqlconn);
+                deletar.Parameters.AddWithValue("@Id_Cli_Rede", cr.Id_cli_rede);
+
+                conn.sqlconn.Dispose();
+                conn.fecharConexao();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+                throw new Exception("Não foi possivel deletar usuário!!");
+            }
+        }
+        public List<ClienteRede> listarClienteRede()
+        {
+            List<ClienteRede> lista = new List<ClienteRede>();
+            try
+            {
+                string sql = "select * from Cli_Rede";
+                SqlCommand cmd = new SqlCommand(sql, conn.sqlconn);
+                SqlDataReader listar = cmd.ExecuteReader();
+
+                while (listar.Read())
+                {
+                    ClienteRede cr = new ClienteRede(listar.GetInt16(listar.GetOrdinal("Id_cli_rede")),
+                    listar.GetString(listar.GetOrdinal("nome_Cli_Rede")),
+                    listar.GetString(listar.GetOrdinal("login_Cli_Rede")),
+                    listar.GetString(listar.GetOrdinal("senha_Cli_Rede")),
+                    listar.GetString(listar.GetOrdinal("email_Cli_Rede")),
+                    listar.GetString(listar.GetOrdinal("telefone_Cli_Rede")));
+                    lista.Add(cr);
+                }
+                cmd.Dispose();
+                conn.fecharConexao();
+
+
+            }
+            catch (Exception)
+            {
+                throw new Exception("Não foi possivel listar os usuários cadastrados!");
+            }
+            return lista;
+        }
+    }
+
+}
---
+++ ProjetoEasyImoveis/WcfServiceClienteRede/classebasica/ClienteRede.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfServiceClienteRede.classebasica
+{
+    public class ClienteRede
+    {
+        private int id_cli_rede;
+
+        public int Id_cli_rede
+        {
+            get { return id_cli_rede; }
+            set { id_cli_rede = value; }
+        }
+        private string nome_cli_rede;
+
+        public string Nome_cli_rede
+        {
+            get { return nome_cli_rede; }
+            set { nome_cli_rede = value; }
+        }
+        private string login_cli_rede;
+
+        public string Login_cli_rede
+        {
+            get { return login_cli_rede; }
+            set { login_cli_rede = value; }
+        }
+        private string senha_cli_rede;
+
+        public string Senha_cli_rede
+        {
+            get { return senha_cli_rede; }
+            set { senha_cli_rede = value; }
+        }
+        private string email_cli_rede;
+
+        public string Email_cli_rede
+        {
+            get { return email_cli_rede; }
+            set { email_cli_rede = value; }
+        }
+        private string telefone_cli_rede;
+
+        public string Telefone_cli_rede
+        {
+            get { return telefone_cli_rede; }
+            set { telefone_cli_rede = value; }
+        }
+
+        //construtor
+        public ClienteRede(int Id_cli_rede, string Nome_cli_rede, string Login_cli_rede, string Senha_cli_rede, string Email_cli_rede, string Telefone_cli_rede)
+        {
+            this.Id_cli_rede = Id_cli_rede;
+            this.Nome_cli_rede = Nome_cli_rede;
+            this.Login_cli_rede = Login_cli_rede;
+            this.Senha_cli_rede = Senha_cli_rede;
+            this.Email_cli_rede = Email_cli_rede;
+            this.Telefone_cli_rede = Telefone_cli_rede;
+        }
+    }
+}
